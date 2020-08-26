be9063d39834eee4eb5fe3890440eda04c5819e6
NeoHuncho <william.guinaudie@gmail.com>
2020-08-26 16:18:34 +0200

    changes

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const randomQuotes=["In all moments, for all reasons--love complete","If we aim high and fall short, we still achieve more than by aiming low and falling short"," May you recognize the passion and the possibility in your day today and have the courage to follow it","When was the last time worry ever solved a problem? Breathe and know that you are perfect in this moment","There is nothing more important in the world than loving yourself"];
-$("#new-quote").click(function (e) { 
-    $("#text").text("hello");
-    
-});
-//randomQuotes[math.floor(Math.random() * randomQuotes.length)]
-let additon= 2+3;
+const chooseQuote= randomQuotes[(Math.floor(Math.random() * randomQuotes.length))]
+function quote(){
+    $("#text").html(chooseQuote);
+}
+$(document).ready(function() { 
+ 
+    $('#new-quote').click(function () { 
+       quote()
+        
+    });
+        
+    });
+           
+   
+        
+
+
+
+
+console.log(chooseQuote);
