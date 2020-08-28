be063fec0c3e909a6f8c3a9a8034a6af9a01ffff
NeoHuncho <william.guinaudie@gmail.com>
2020-08-28 17:32:43 +0200

    Finished twitter link+ added new icon

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.scss';
 import Button from 'react-bootstrap';
 import Grid from '@material-ui/core/Grid';
 import { render } from '@testing-library/react';
+import {SocialMediaIconsReact} from 'social-media-icons-react';
 import {faTwitter} from '@fortawesome/free-solid-svg-icons';
 const colors= ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
 const API= 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
@@ -66,8 +67,8 @@ return (
         {quote && <h2 className="card-title" id="author">{quote.author}</h2>}
         <div className="d-flex justify-content-between" id="buttons">
         <button  type="button" id="new-quote" onClick={() => { this.getRandomIndex(); this.getRandomColorIndex()}} className="btn btn-primary">Next quote</button>      
-         
-         <a class="twitter-share-button" href={tweetURl} id="tweet-quote"><i class="fab fa-twitter"></i>Tweet</a></div>    
+       <SocialMediaIconsReact url={tweetURl} id="tweet-quote" borderColor={colorChoosen} borderWidth="5" borderStyle="solid" icon="twitter" iconColor="rgba(255,255,255,1)" backgroundColor={colorChoosen} iconSize="5" roundness="20%"  size="50" />
+         </div>    
       </section>
       </div>
 </body>
@@ -75,7 +76,7 @@ return (
 );}
 }
 
-
+//<a href={tweetURl} id="tweet-quote">Tweet</a> This is to chave 12 out of 12 on the FFC project. Replace other twitter icon
 /*function App() {
   return (
     <body>
