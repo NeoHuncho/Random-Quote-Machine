9aa2c12baaf0d4a312aaa383565a24ae1ffc2729
NeoHuncho <william.guinaudie@gmail.com>
2020-08-28 11:11:39 +0200

    cleaned up UI, using scss

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.scss';
 import Button from 'react-bootstrap';
 import Grid from '@material-ui/core/Grid';
 import { render } from '@testing-library/react';
+const colors= ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
 const API= 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
-
 class App extends React.Component {
   constructor(props) {
     super(props);
          this.state={
-      color:['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"],
+      color:colors,
         quotes:[],
         index:0,
         colorIndex:0
@@ -49,27 +49,23 @@ const colorChoosen=color[colorIndex];
 const styleObj={
   background:colorChoosen
 }
-console.log(colorChoosen)
+
 return (
  
   <body style={styleObj} className="container">
-    
-    <style>{'body { background-color: color; }'}</style>
-  <section id="quote-machine" className="centered">
+    <div className="blockquote-wrapper"id="quote-box">
+     <section id="quote-machine" className="centered">
       <div id="title">
          <h1 className="title">Random Quote Machine</h1> 
       </div>
-      <div id="quote-box" class="card">
+     
         {quote && <p className="card-text" id="text">{quote.quote}</p>}
         {quote && <h2 className="card-title" id="author">{quote.author}</h2>}
-      </div>
-      <div className="d-flex justify-content-between">
+        <div className="d-flex justify-content-between" id="buttons">
         <button  type="button" id="new-quote" onClick={() => { this.getRandomIndex(); this.getRandomColorIndex()}} className="btn btn-primary">Next quote</button>      
-         <button className="btn btn-primary" id="tweet-quote">Tweet</button> 
-       </div>
-        
-       
+         <button className="btn btn-primary" id="tweet-quote">Tweet</button></div>    
       </section>
+      </div>
 </body>
 
 );}
